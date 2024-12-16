63e048f36683cee5bd361293965c25eac3aed6dd
Egor <repingign@gmail.com>
2024-12-16 22:16:34 +0500

    Add files via upload

---
+++ Интроспекция.py
@@ -0,0 +1,17 @@
+import inspect
+
+
+def introspection_info(obj):
+    attributes_list, methods_list = [], []
+    for element in dir(obj):
+        if '__' in element:
+            methods_list.append(element)
+        else:
+            attributes_list.append(element)
+    info_dict = {'type': type(obj).__name__, 'attributes': attributes_list, 'methods': methods_list,
+                 'module': inspect.getmodule(introspection_info).__name__}
+    return info_dict
+
+
+number_info = introspection_info(42)
+print(number_info)
